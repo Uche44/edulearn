ac6dfa4905699fa4a071cc8d3f1f94262505674a
Uche44 <perpetualuchechukwu808@gmail.com>
2025-10-08 00:04:59 +0100

    fix: correct DJANGO_SETTINGS_MODULE path in WSGI configuration

--- backend/learnthru/learnthru/wsgi.py
+++ backend/learnthru/learnthru/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learnthru.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learnthru.learnthru.settings')
 
 application = get_wsgi_application()
