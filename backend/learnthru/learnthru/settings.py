cec18b1229a352419ddc4744291c17d6f435be2c
Uche44 <perpetualuchechukwu808@gmail.com>
2025-10-08 16:15:18 +0100

    fix: update INSTALLED_APPS to use the correct API path

--- backend/learnthru/learnthru/settings.py
+++ backend/learnthru/learnthru/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'learnthru.api',
+    'api',
     'rest_framework',
     'corsheaders'
 ]
